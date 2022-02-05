8e79c3082993a15d5e0179c365102ff971fef199
Chaz Simons <chaz.simons@gmail.com>
2022-02-05 15:57:21 -0800

    Add: farmer delete mutation

---
+++ app/graphql/mutations/delete_farmer.rb
@@ -0,0 +1,13 @@
+module Mutations
+  class DeleteFarmer < BaseMutation
+    field :id, ID, null: true
+
+    argument :id, ID, required: false
+
+    def resolve(id:)
+      farmer = Farmer.find(id)
+      farmer.destroy
+      { id: id }
+    end
+  end
+end
--- app/graphql/types/mutation_type.rb
+++ app/graphql/types/mutation_type.rb
@@ -1,6 +1,9 @@
 module Types
   class MutationType < Types::BaseObject
     field :create_farmer, mutation: Mutations::CreateFarmer
+
     field :update_farmer, mutation: Mutations::UpdateFarmer
+
+    field :delete_farmer, mutation: Mutations::DeleteFarmer
   end
 end
