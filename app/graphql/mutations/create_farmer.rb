02d83cfeb47b02ec4a4b84b47317af82e1356578
Philip Medlin <phmedlin@gmail.com>
2022-02-15 12:01:22 -0700

    Change to photos I own

--- app/graphql/mutations/create_farmer.rb
+++ app/graphql/mutations/create_farmer.rb
@@ -28,17 +28,16 @@ module Mutations
     end
 
     def random_farm_image
-      array = ["https://images.unsplash.com/photo-1444858291040-58f756a3bdd6?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyOTE4MDZ8MHwxfHNlYXJjaHwxfHxmYXJtfGVufDB8fHx8MTY0NDk0NjUwNA&ixlib=rb-1.2.1&q=80&w=1080",
-               "https://images.unsplash.com/photo-1523741543316-beb7fc7023d8?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyOTE4MDZ8MHwxfHNlYXJjaHwyfHxmYXJtfGVufDB8fHx8MTY0NDk0NjUwNA&ixlib=rb-1.2.1&q=80&w=1080",
-               "https://images.unsplash.com/photo-1500382017468-9049fed747ef?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyOTE4MDZ8MHwxfHNlYXJjaHwzfHxmYXJtfGVufDB8fHx8MTY0NDk0NjUwNA&ixlib=rb-1.2.1&q=80&w=1080",
-               "https://images.unsplash.com/photo-1560493676-04071c5f467b?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyOTE4MDZ8MHwxfHNlYXJjaHw0fHxmYXJtfGVufDB8fHx8MTY0NDk0NjUwNA&ixlib=rb-1.2.1&q=80&w=1080",
-               "https://images.unsplash.com/photo-1499529112087-3cb3b73cec95?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyOTE4MDZ8MHwxfHNlYXJjaHw1fHxmYXJtfGVufDB8fHx8MTY0NDk0NjUwNA&ixlib=rb-1.2.1&q=80&w=1080",
-               "https://images.unsplash.com/photo-1495107334309-fcf20504a5ab?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyOTE4MDZ8MHwxfHNlYXJjaHw2fHxmYXJtfGVufDB8fHx8MTY0NDk0NjUwNA&ixlib=rb-1.2.1&q=80&w=1080",
-               "https://images.unsplash.com/9/fields.jpg?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyOTE4MDZ8MHwxfHNlYXJjaHw3fHxmYXJtfGVufDB8fHx8MTY0NDk0NjUwNA&ixlib=rb-1.2.1&q=80&w=1080",
-               "https://images.unsplash.com/photo-1515150144380-bca9f1650ed9?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyOTE4MDZ8MHwxfHNlYXJjaHw4fHxmYXJtfGVufDB8fHx8MTY0NDk0NjUwNA&ixlib=rb-1.2.1&q=80&w=1080",
-               "https://images.unsplash.com/photo-1454179083322-198bb4daae41?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyOTE4MDZ8MHwxfHNlYXJjaHw5fHxmYXJtfGVufDB8fHx8MTY0NDk0NjUwNA&ixlib=rb-1.2.1&q=80&w=1080",
-               "https://images.unsplash.com/photo-1500076656116-558758c991c1?crop=entropy&cs=tinysrgb&fit=max&fm=jpg&ixid=MnwyOTE4MDZ8MHwxfHNlYXJjaHwxMHx8ZmFybXxlbnwwfHx8fDE2NDQ5NDY1MDQ&ixlib=rb-1.2.1&q=80&w=1080"]
+      array = ["https://imgur.com/0thgtTJ",
+              "https://imgur.com/ettFY8W",
+              "https://imgur.com/r2d8r4n",
+              "https://imgur.com/pGEOMvT",
+              "https://imgur.com/8TbD8YX",
+              "https://imgur.com/DR0gmaA",
+              "https://imgur.com/RJqNDM8",
+              "https://imgur.com/eJvXanO"]
+
       array.shuffle.pop(1).first
     end
   end
-end
+endgit
